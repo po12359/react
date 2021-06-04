a53bd34c219981271f6ff0cbabf9a7fef7806f50
po12359 <79793382+po12359@users.noreply.github.com>
2021-06-05 03:37:25 +0900

    habit-first-day

---
+++ react-basic/habit-tracker/src/app.jsx
@@ -0,0 +1,17 @@
+import "./app.css";
+import Habit from "./components/habit";
+
+function App() {
+  return (
+    <Habit></Habit>
+    // 자식 요소가 있을 시
+    //   <habit />
+    // 자식 요소가 없을 시
+    /*   <>
+    <h1>nono</h1>
+    <h1>nono</h1>
+    */
+  );
+}
+
+export default App;
--- react-basic/template/src/app.jsx
+++ react-basic/template/src/app.jsx
@@ -1,7 +1,20 @@
 import "./app.css";
 
 function App() {
-  return <h1>Hello ")</h1>;
+  const name = `Hello`;
+  return (
+    <>
+      <h1>Hello {name}</h1>
+      {name && <h1>nice to meet you {name}</h1>}
+      {[`🍎`, `🍑`].map((item) => (
+        <h1>{item}</h1>
+      ))}
+    </>
+    /*   <>
+    <h1>nono</h1>
+    <h1>nono</h1>
+    </>*/
+  );
 }
 
 export default App;
